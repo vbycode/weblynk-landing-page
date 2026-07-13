52297e3291c2f6533db3ca1cee6b8498225a6e02
Vaibhav Tyagi <tyagivaibhav726@gmail.com>
2026-07-13 13:34:45 +0530

    Added HTML, CSS, and JavaScript

---
+++ Script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
