4c0c1c5ab4da83edddbecf1dc42ac06d713f60e2
Vaibhav Tyagi <tyagivaibhav726@gmail.com>
2026-07-13 15:33:36 +0530

    Added Features section

--- Script.js
+++ Script.js
@@ -1,6 +1,21 @@
 const menu = document.querySelector(".menu");
 const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links a");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", (e) => {
+  e.stopPropagation(); // Prevent document click from firing
   navLinks.classList.toggle("active");
+});
+
+// Close when clicking anywhere outside
+document.addEventListener("click", (e) => {
+  if (navLinks.classList.contains("active") && !navLinks.contains(e.target)) {
+    navLinks.classList.remove("active");
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
